5be2f9c75088f67919f0e478f9f0d2475586192d
Bryce? <bryce.jones2026@gbwl.org>
2025-09-10 13:40:50 +0000

    START

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("hello world")
+print("supercalifragilisticexpialidocious!")
 
+# Variables store data
 
-if __name__ == "__main__":
-    main()
+lucky_num = 67 #int
+my_name = "Bryce?" #str
+cash = 5.50 #float
+is_rain = True #boolean
+is_Sunny = False
